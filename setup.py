fdcea4ae8123d9ade268922fc22dd5962081ef8a
Kasun-Tharaka <k.tharakadharmapriya@gmail.com>
2022-06-25 16:20:07 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,35 +1,32 @@
+
+"""
 from importlib.resources import Package
 from lib2to3.pgen2.token import NAME
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 import re
 
 
 #declaring variables for setup function
 PROJECT_NAME = 'housing predictor'
-VERSION = '0.0.1'
+VERSION = '0.0.3'
 AUTHOR = 'Kasun Tharaka'
 DESCRIPTION = 'this is a testing house pricing project'
-PACKAGES = ['housing']
+PACKAGES = find_packages()
 REQUIREMENT_FILE_NAME = 'requirements.txt'
 
 
+
 def get_requirements_list()->List[str]:
-    """
-    Description: This functio is going to return requirement 
-    mentioned requirement.txt file
+   
+    # Description: This functio is going to return requirement 
+    # mentioned requirement.txt file
     
-    returns this functio is going to return list which will content name 
-    of libraries mentioned in requirement.txt file
-    """
+    # returns this functio is going to return list which will content name 
+    # of libraries mentioned in requirement.txt file
+  
     with open(REQUIREMENT_FILE_NAME) as requirment_file:
-        
-        # my_str = requirment_file.read()
-        # m = re.findall(r'\w', my_str)
-        # return m
-        
-        m = requirment_file.readlines()
-        return m
+        return requirment_file.readlines().remove('-e .')
 
 
 setup(
@@ -40,3 +37,25 @@ setup(
     package = PACKAGES,
     install_requires = get_requirements_list()
 )
+"""
+
+"""
+why -e remove, 
+    -e and find_packages are doing same job. 
+    -e job is describ in requirement.txt file.
+    find_package() will find all of libraries inside our project and then install.
+    so now we have find_package() function, so we don't need -e, then you can remove it dinamically
+"""
+
+
+""""
+ this seetup.py is works well on Linux, in Windows some kind of issure, 
+ so in terminal pip install -r requirement.txt
+ """
+
+
+"""
+ why we are using setup.py file, when we need to deploy 
+ this project as a library then we use this file.
+ """
+
