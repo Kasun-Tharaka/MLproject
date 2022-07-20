785b51e7eb5fbc4f33a26e15f149f75cca70f353
Kasun-Tharaka <k.tharakadharmapriya@gmail.com>
2022-07-20 10:06:46 +0530

    complete project

--- housing/exception/__init__.py
+++ housing/exception/__init__.py
@@ -1,43 +1,37 @@
 import os
 import sys
 
-#declair the parent vlass
 class HousingException(Exception):
     
-    #error_message wil be Object of Exception class, and error_details will be Object of sys class. 
-    # so we can capture all of details through objects
-    def __init__(self, error_message:Exception, error_details:sys):
-        #passing the error message to Exception class
+    def __init__(self, error_message:Exception,error_detail:sys):
         super().__init__(error_message)
-        self.error_message = HousingException.get_detail_error_message(self, error_message=error_message,
-                                                                        error_details=error_details)
+        self.error_message=HousingException.get_detailed_error_message(error_message=error_message,
+                                                                       error_detail=error_detail
+                                                                        )
 
 
-    #using this we can call this function without creating any object
     @staticmethod
-    def get_detail_error_message(self, error_message:Exception, error_details:sys)->str:
+    def get_detailed_error_message(error_message:Exception,error_detail:sys)->str:
         """
-        error_message : Exception object
-        error_detail : object of sys module
+        error_message: Exception object
+        error_detail: object of sys module
         """
-        _,_ , exec_tb = error_details.exc_info()
-
-        # getting line number of the error
-        line_number = exec_tb.tb_frame.f_lineno
-
-        # getting file name of the error
+        _,_ ,exec_tb = error_detail.exc_info()
+        exception_block_line_number = exec_tb.tb_frame.f_lineno
+        try_block_line_number = exec_tb.tb_lineno
         file_name = exec_tb.tb_frame.f_code.co_filename
-
-        error_message = f"Error occured in script: [{file_name}] at line number: [{line_number}] error message: [{error_message}]"
-
+        error_message = f"""
+        Error occured in script: 
+        [ {file_name} ] at 
+        try block line number: [{try_block_line_number}] and exception block line number: [{exception_block_line_number}] 
+        error message: [{error_message}]
+        """
         return error_message
 
-
-    # if do you want to print object of ant class as print statement, 
-    # use Dunder method to print it as a function
     def __str__(self):
         return self.error_message
 
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return HousingException.__name__.str()
+
--- setup.py
+++ setup.py
@@ -1,61 +1,39 @@
-
-"""
-from importlib.resources import Package
-from lib2to3.pgen2.token import NAME
-from setuptools import setup, find_packages
+from setuptools import setup,find_packages
 from typing import List
-import re
+
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.3"
+AUTHOR="Avnish Yadav"
+DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
 
 
-#declaring variables for setup function
-PROJECT_NAME = 'housing predictor'
-VERSION = '0.0.3'
-AUTHOR = 'Kasun Tharaka'
-DESCRIPTION = 'this is a testing house pricing project'
-PACKAGES = find_packages()
-REQUIREMENT_FILE_NAME = 'requirements.txt'
+def get_requirements_list() -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
 
-
-
-def get_requirements_list()->List[str]:
-   
-    # Description: This functio is going to return requirement 
-    # mentioned requirement.txt file
-    
-    # returns this functio is going to return list which will content name 
-    # of libraries mentioned in requirement.txt file
-  
-    with open(REQUIREMENT_FILE_NAME) as requirment_file:
-        return requirment_file.readlines().remove('-e .')
 
 
 setup(
-    name = NAME,
-    version = VERSION,
-    author = AUTHOR,
-    description = DESCRIPTION,
-    package = PACKAGES,
-    install_requires = get_requirements_list()
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(), 
+install_requires=get_requirements_list()
 )
-"""
-
-"""
-why -e remove, 
-    -e and find_packages are doing same job. 
-    -e job is describ in requirement.txt file.
-    find_package() will find all of libraries inside our project and then install.
-    so now we have find_package() function, so we don't need -e, then you can remove it dinamically
-"""
-
-
-""""
- this seetup.py is works well on Linux, in Windows some kind of issure, 
- so in terminal pip install -r requirement.txt
- """
-
-
-"""
- why we are using setup.py file, when we need to deploy 
- this project as a library then we use this file.
- """
 
